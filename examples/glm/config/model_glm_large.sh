c885d96e7aef852d9d9a964c0300aca685e7bbd3
tsm <smeyerhot@gmail.com>
2022-12-27 23:00:06 +0000

    Fix small model training

--- examples/glm/config/model_glm_large.sh
+++ examples/glm/config/model_glm_large.sh
@@ -1,11 +1,16 @@
+prefix_len=10
+max_seq_len=512
+sample_len="$((max_seq_len-prefix_len-1))"
 MODEL_TYPE="blocklm-large"
 MODEL_ARGS="--block-lm \
             --cloze-eval \
-            --prefix_len 20 \
+            --task-mask \
+            --prefix_len $prefix_len \
+            --sample_len $sample_len \
+            --max-sequence-length $max_seq_len \
             --num-layers 24 \
             --hidden-size 1024 \
             --num-attention-heads 16 \
-            --max-sequence-length 513 \
             --tokenizer-type glm_GPT2BPETokenizer \
-            --tokenizer-model-type glm-large \
+            --tokenizer-model-type gpt2 \
             --load ${CHECKPOINT_PATH}/glm-large-en-blank"
--- examples/glm/finetune_reg_small.py
+++ examples/glm/finetune_reg_small.py
@@ -5,6 +5,7 @@ import os
 import sys
 import math
 import random
+import gc 
 
 from SwissArmyTransformer.data_utils.datasets import TSVDataset
 import torch
@@ -22,26 +23,33 @@ from SwissArmyTransformer.model.mixins import MLPHeadMixin, PrefixTuningMixin, C
 class AutoregressiveModel(GLMModel):
     def __init__(self, args, transformer=None, parallel_output=True):
         super().__init__(args, transformer=transformer, parallel_output=parallel_output)
-        # self.add_mixin('auto-regressive', CachedAutoregressiveMixin())
-        self.add_mixin('classification_head', MLPHeadMixin(args.hidden_size, 1024, 1))
-        print(args.num_layers)
-        print(args.hidden_size)
-        print(args.num_attention_heads)
-        print(args.prefix_len)
         self.add_mixin('prefix-tuning', PrefixTuningMixin(args.num_layers, args.hidden_size // args.num_attention_heads, args.num_attention_heads, args.prefix_len))
-        print(args.num_layers)
-        print(args.hidden_size)
-        print(args.num_attention_heads)
-        print(args.prefix_len)
 
     def disable_untrainable_params(self):
         self.transformer.word_embeddings.requires_grad_(False)
         for layer_id in range(len(self.transformer.layers)):
             self.transformer.layers[layer_id].requires_grad_(False)
 
+def get_masks(data, loss_mask=None, attention_mask=None, args=None):
+    # Extract batch size and sequence length.
+    batch_size, seq_length = data.size()
+
+    # Attention mask (lower triangular).
+    if attention_mask is None:
+        attention_mask = torch.ones((batch_size, seq_length, seq_length), device=data.device)
+        attention_mask.tril_()
+        attention_mask.unsqueeze_(1)
+        
+    # Loss mask.
+    if loss_mask is None:
+        loss_mask = torch.ones(data.size(), dtype=data.dtype, device=data.device)
+
+    return attention_mask, loss_mask
+
+
 def get_batch(data_iterator, args, timers):
     # Items and their type.
-    keys = ['sentence', 'label']
+    keys = ['text', 'loss_mask']
     datatype = torch.int64
 
     # Broadcast data.
@@ -51,24 +59,35 @@ def get_batch(data_iterator, args, timers):
     else:
         data = None
     timers('data loader').stop()
+
     data_b = mpu.broadcast_data(keys, data, datatype)
-    print("data_b", data_b)
     # Unpack.
-    tokens = data_b['sentence'].long()
-    labels = data_b['label'].long()
-    batch_size, seq_length = tokens.size()
+    tokens_ = data_b['text'].long()
+    loss_mask = data_b['loss_mask'].float()
     
+    labels = tokens_[:, 1:].contiguous()
+    loss_mask = loss_mask[:, 1:].contiguous()
+    tokens = tokens_[:, :-1].contiguous()
+    batch_size, seq_length = tokens.size()
+
     position_ids = torch.zeros(2, seq_length, device=tokens.device, dtype=torch.long)
     torch.arange(0, seq_length, out=position_ids[0, :seq_length])
     position_ids = position_ids.unsqueeze(0)
-    
-    attention_mask = torch.ones((batch_size, 1, seq_length, seq_length), device=tokens.device)
 
-    attention_mask[...,:seq_length] -= (tokens==-1).view(batch_size, 1, 1, seq_length).float()
+    attention_mask = None        
+
+    # Get the masks and postition ids.
+    attention_mask, loss_mask = get_masks(
+        tokens,
+        loss_mask=loss_mask,
+        attention_mask=attention_mask,
+        args=args
+        )
     # Convert
     if args.fp16:
         attention_mask = attention_mask.half()
-    return tokens, labels, attention_mask, position_ids, (tokens!=-1)
+
+    return tokens, labels, loss_mask, attention_mask, position_ids 
 
 
 def forward_step(data_iterator, model, args, timers):
@@ -76,40 +95,46 @@ def forward_step(data_iterator, model, args, timers):
 
     # Get the batch.
     timers('batch generator').start()
-    tokens, labels, attention_mask, position_ids, loss_mask = get_batch(
+    tokens, labels, loss_mask, attention_mask, position_ids = get_batch(
         data_iterator, args, timers)
     timers('batch generator').stop()
-
+    # Forward model.
+    print(tokens.shape)
+    print(position_ids.shape)
     logits, *mems = model(tokens, position_ids, attention_mask)
-    pred = ((logits.contiguous().float().squeeze(-1)) * loss_mask).sum(dim=-1) / loss_mask.sum(dim=-1)
-    loss = torch.nn.functional.binary_cross_entropy_with_logits(
-        pred, 
-        labels.float()
-        )
-    acc = ((pred > 0.).long() == labels).sum() / labels.numel()
-    return loss, {'acc': acc}
+    losses = mpu.vocab_parallel_cross_entropy(logits.contiguous().float(), labels)
+    # scaling loss mask
+    loss_mask = loss_mask.view(-1)  
+
+    losses = losses.view(-1) * loss_mask
+    loss = torch.sum(losses) / loss_mask.sum()
+    return loss, {}
 
 def create_dataset_function(path, args):
     tokenizer = get_tokenizer(args)
-
+    
     def process_fn(row):
         dialog = row['dialog']
-        text = '\n'.join(dialog)
-                   
-        sentence = tokenizer._encode(text[1:])
+        # TODO: Consider changing how text is preprocessed
+        text = ''.join(dialog)  
+        sentence = tokenizer._encode(text[:])
         sentence = sentence + [tokenizer.get_command('eos').Id]
+        loss_mask = [1] * args.sample_length
         if len(sentence) >= args.sample_length:
-            sentence = sentence[args.sample_length - len(sentence):]
+            # Cutoff on rhs
+            sentence = sentence[:args.sample_length]
         else:
-    
-            sentence.extend([-1] * (args.sample_length-len(sentence)))
-        return {'sentence': np.array(sentence, dtype=np.int64)}
-    return load_hf_dataset(path, process_fn, columns=['sentence'], offline=False)
+            n_pad = args.sample_length-len(sentence)
+            loss_mask = np.array([1]*len(sentence) + [0]*n_pad)
+            sentence.extend([-1] * n_pad)
 
-if __name__ == '__main__':    
+        return {'text': np.array(sentence, dtype=np.int64), 'loss_mask': np.array(loss_mask, dtype=np.int64)}
+    return load_hf_dataset(path, process_fn, columns=['text', 'loss_mask'], cache_dir='~/dataset/SwissArmyTransformerDatasets', offline=False)
+
+if __name__ == '__main__':   
     py_parser = argparse.ArgumentParser(add_help=False)
     py_parser.add_argument('--new_hyperparam', type=str, default=None)
-    py_parser.add_argument('--sample_length', type=int, default=80)
+    py_parser.add_argument('--sample_length', type=int, default=512-16)
     py_parser.add_argument('--prefix_len', type=int, default=16)
     GLMModel.add_model_specific_args(py_parser)
     known, args_list = py_parser.parse_known_args()
